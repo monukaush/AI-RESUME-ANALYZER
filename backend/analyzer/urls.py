0b87eb66378264dc0363f971946ab2a81d4eb796
monukaush <kaushikmonu851@gmail.com>
2026-07-15 20:31:08 +0530

    Add backend Django structure, update package.json, and update .gitignore

---
+++ backend/analyzer/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from .views import test_connection
+
+
+urlpatterns = [
+    path(
+        "test/",
+        test_connection,
+        name="test-connection",
+    ),
+]
