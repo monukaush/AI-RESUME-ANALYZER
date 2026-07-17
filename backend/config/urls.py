da877c06cde7b7353f432543e4276a498ce18016
monukaush <kaushikmonu851@gmail.com>
2026-07-17 23:07:25 +0530

    Fix admin urls path config

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.name if hasattr(admin.site, 'name') else admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/', include('analyzer.urls')), 
 ]
